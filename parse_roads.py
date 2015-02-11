d0882e5b5351c70ccf8bc96c46af83f27de20b65
Geoff Cowan <geoffroy.cowan@gmail.com>
2015-02-11 14:57:05 +1100

    Moved in/out paths to command line args.

--- parse_roads.py
+++ parse_roads.py
@@ -1,8 +1,14 @@
+import sys
+
 points = []
 pairs = []
 links = {}
 
-with open("input.txt") as f:
+input_path = sys.argv[1]
+output_path = sys.argv[2]
+ignore_one_way = (sys.argv[3] == 'true')
+
+with open(input_path) as f:
   for line in f:
     direction = line[1]
     line_points = line.strip()[13:-1].split(',')
@@ -11,7 +17,7 @@ with open("input.txt") as f:
     for i in range(len(line_points)-1):
       pairs.append([line_points[i], line_points[i+1]])
       #TWO WAY STREET
-      if (direction == 'B'):
+      if (direction == 'B' or ignore_one_way):
         pairs.append([line_points[i+1], line_points[i]])
 
 points = list(set(points))
@@ -22,7 +28,7 @@ for point in points:
       point_links.append(pair[1])
   links[point] = point_links
 
-with open('output.txt', 'a') as f:
+with open(output_path, 'a') as f:
   for point in points:
     f.write(point + '\n')
     f.write(str(links[point])+'\n')
